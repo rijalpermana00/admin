9c7752224911077168c361d8a3a7812d493da40e
rijal <rijal@neuronworks.co.id>
2021-01-22 23:40:06 +0700

    adding router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import { ActiveProvider } from './components/ActiveContext'
+import Header from './components/layout/Header'
+import Navbar from './components/layout/Navbar'
+import Overlay from './components/layout/Overlay'
+import { ActiveProvider } from './components/layout/ActiveContext'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Main from './components/main/Main'
+import About from './components/about/About'
 
 function App() {
     return (
-        <ActiveProvider>
-            <div className="wrapper">
-                <Navbar/>
-                <div id="content">
-                    <Header/>
+        <Router>
+            <ActiveProvider>
+                <div className="wrapper">
+                    <Navbar/>
+                    <div id="content">
+                        <Overlay/>
+                        <Header/>
+                        <main className="ml-sm-auto col-12 pt-3 px-4">
+                            <Switch>
+                                <Route path="/" exact component={Main}/>
+                                <Route path="/about" component={About}/>
+                            </Switch>
+                        </main>
+                    </div>
                 </div>
-            </div>
-        </ActiveProvider>
+            </ActiveProvider>
+        </Router>
     );
 }
 
